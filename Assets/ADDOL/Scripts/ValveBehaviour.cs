b5bff336677770b73eeecb54c56d315ab54f5c3b
Jordo <jordan.his@viacesi.fr>
2019-01-08 12:21:11 +0100

    Multiplayer added

---
+++ Assets/ADDOL/Scripts/ValveBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ValveBehaviour : NetworkBehaviour {
+
+
+	public GameObject particules;
+	public GameObject valveToReplace;
+	
+	public float minAngle = 0;
+	public float maxAngle = 720;
+
+	public float CurrentAngle = 0;
+	public Vector3 lastEuler;
+	void Start()
+	{
+		lastEuler = gameObject.transform.rotation.eulerAngles;
+	}
+
+	//Si la rotation en X est -720, alors changer tag de la valve + faire disparaitre particules.
+	void Update()
+	{
+		float angle = gameObject.transform.rotation.eulerAngles.x - lastEuler.x;
+		CurrentAngle += angle;
+		lastEuler = gameObject.transform.rotation.eulerAngles;
+		//valveRotation();
+	}
+
+	[ClientRpc]
+	public void RpcRotationComplete()
+	{
+		
+	}
+
+
+
+}
---
+++ Assets/IncidentManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncidentManager : MonoBehaviour {
+
+	[HeaderAttribute( "Fire incident" )]
+	public ParticleSystem Fire;
+	public GameObject SafetyRoute;
+
+	public void FireIncident()
+	{
+		Fire.Play();
+		SafetyRoute.SetActive(true);
+	}
+
+}
